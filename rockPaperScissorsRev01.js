b1dd5a9484561bef6be6efe5429293eb4a809171
magiAssenova <magi_assenova@yahoo.com>
2022-09-27 15:59:56 +0300

    Create rockPaperScissorsRev01.js

---
+++ rockPaperScissorsRev01.js
@@ -0,0 +1,43 @@
+function rockPpaerScissors(playerturn){
+    const rock="Rock";
+    const paper="Paper";
+    const scissors="Scissors";
+    if (playerturn=="r" || playerturn=="rock"){
+        playerturn=rock
+    }
+    else if (playerturn=='s' || playerturn=="scissors"){
+        playerturn=scissors
+    }
+    else if (playerturn=="p"||playerturn=="paper"){
+        playerturn=paper
+    }
+    else{
+        console.log("Invalid Input.Try again...")
+    }
+    let computerTurn=Math.floor(Math.random()*3)+1
+
+switch (computerTurn) {
+    case 1:
+        computerTurn=rock
+        break;
+        case 2:
+            computerTurn=paper
+            break;
+            case 3:
+                computerTurn=scissors
+                break;}
+console.log(`You choose ${playerturn}`)
+console.log(`Computer choses ${computerTurn}`)
+if ((playerturn===rock && computerTurn===rock) || (playerturn==scissors && computerTurn==scissors) ||
+(playerturn==paper && computerTurn==paper)){
+    console.log("This game was a dwaw!")
+}
+else if ((playerturn===rock && computerTurn==scissors) || (playerturn==paper && computerTurn==rock)||(playerturn==scissors && computerTurn==paper)){
+    console.log("You win!")
+}
+else {
+    console.log("You loose!")
+}
+
+}
+rockPpaerScissors("s")
